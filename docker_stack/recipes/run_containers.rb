c5d58ec7b25761b1089b9f7945c982c40ac4de45
Johannes Boyne <johannes@boyne.de>
2015-01-03 20:36:32 +0100

    Update recipe

--- docker_stack/recipes/run_containers.rb
+++ docker_stack/recipes/run_containers.rb
@@ -1,4 +1,7 @@
 # Run each app. We don't expose any ports since Nginx will handle all incoming traffic as a proxy
+node[:environment_variables].each do |name, value|
+    ENV["#{name}"] = "#{value}"
+end
 node[:my_apps].each do |name, image|  
   script "run_app_#{name}_container" do
     interpreter "bash"
