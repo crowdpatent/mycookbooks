a8fc482d750834b1d0ea77a65a2105f6e865a073
Johannes Boyne <johannes@boyne.de>
2015-01-03 17:55:29 +0100

    Update recipe

--- docker_stack/recipes/pull_images.rb
+++ docker_stack/recipes/pull_images.rb
@@ -1,11 +1,11 @@
 # Pull each of our defined apps
-node[:my_apps].each do |name, image, registry|  
+node[:my_apps].each do |name, image|  
   script "pull_app_#{name}_image" do
     interpreter "bash"
     user "root"
     code <<-EOH
       /etc/init.d/docker stop
-      /etc/init.d/docker start --insecure-registry #{registry}
+      /etc/init.d/docker --insecure-registry #{image.split("/")[0]}
       docker pull #{image}
     EOH
   end
