8a97bf0e99311e36272e3215694d7ea30b7a593b
Johannes Boyne <johannes@boyne.de>
2015-01-03 10:58:06 +0100

    Change recipes

---
+++ docker_stack/recipes/run_containers.rb
@@ -0,0 +1,10 @@
+# Run each app. We don't expose any ports since Nginx will handle all incoming traffic as a proxy
+node[:my_apps].each do |name, image|  
+  script "run_app_#{name}_container" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+      docker run -d -p 3000:3000 --name=#{name} #{image}
+    EOH
+  end
+end
