15c5498e49e636e3c6fc4af992e5a9ee2e89ed47
Johannes Boyne <johannes@boyne.de>
2015-01-19 20:10:36 +0100

    Refactor run container recipe

--- docker_stack/recipes/run_containers.rb
+++ docker_stack/recipes/run_containers.rb
@@ -1,10 +1,16 @@
 # Run each app. We don't expose any ports since Nginx will handle all incoming traffic as a proxy
 node[:my_apps].each do |name, image|  
-  env_vars = node[:environment_variables].map {|k, v| "-e #{k}=#{v}" }.join(" ")
-  puts env_vars
+  env_vars = ""
+  ports    = ""
+  if node[:environment_variables]
+    env_vars = node[:environment_variables].map {|k, v| "-e #{k}=#{v}" }.join(" ")
+    puts env_vars
+  end
 
-  ports = node[:ports].map {|k, v| "-p #{k}=#{v}" }.join(" ")
-  puts ports
+  if node[:ports]
+    ports = node[:ports].map {|k, v| "-p #{k}=#{v}" }.join(" ")
+    puts ports
+  end
 
   script "run_app_#{name}_container" do
     interpreter "bash"
