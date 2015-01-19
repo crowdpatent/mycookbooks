cef75fa2581f077fdcacd003fa03164efaa012e0
Johannes Boyne <johannes@boyne.de>
2015-01-19 19:53:13 +0100

    Updating cookbook to support port settings

--- docker_stack/recipes/run_containers.rb
+++ docker_stack/recipes/run_containers.rb
@@ -2,11 +2,15 @@
 node[:my_apps].each do |name, image|  
   env_vars = node[:environment_variables].map {|k, v| "-e #{k}=#{v}" }.join(" ")
   puts env_vars
+
+  ports = node[:ports].map {|k, v| "-p #{k}=#{v}" }.join(" ")
+  puts ports
+
   script "run_app_#{name}_container" do
     interpreter "bash"
     user "root"
     code <<-EOH
-      docker run -d -p 3000:3000 #{env_vars} --name=#{name} #{image}
+      docker run --restart=on-failure:5 -d #{ports} #{env_vars} --name=#{name} #{image}
     EOH
   end
 end
