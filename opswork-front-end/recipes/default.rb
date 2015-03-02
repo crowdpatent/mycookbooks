3694da4ead9a5e1df58e99a14ba712e5e3a42ecb
Johannes Boyne <johannes@boyne.de>
2015-03-02 18:06:25 +0100

    Change bower and gulp

--- opswork-front-end/recipes/default.rb
+++ opswork-front-end/recipes/default.rb
@@ -1,5 +1,15 @@
 node[:deploy].each do |app, deploy|
    execute "bower_install" do
+        script "bower and gulp" do
+          interpreter "bash"
+          user "root"
+          cwd "/tmp"
+          code <<-EOH
+            npm -q -g install gulp
+            npm -q -g install bower
+          EOH
+        end
+        command "gulp default"
         command "bower install --allow-root"
         cwd "#{deploy[:deploy_to]}/current"
    end
