8e5f25273c753bd618b96b479b83b2f51ad173d7
Johannes Boyne <johannes@boyne.de>
2015-03-03 08:51:21 +0100

    Pimp setup

--- opswork-front-end/recipes/default.rb
+++ opswork-front-end/recipes/default.rb
@@ -4,12 +4,14 @@ node[:deploy].each do |app, deploy|
     user "root"
     command "npm install -g gulp"
     command "npm install -g bower"
+    command "ln -s /usr/bin/nodejs /usr/bin/node"
+    command "echo \"{'interactive': false}\" > ~/.bowerrc"
   end
 
   execute "run gulp and bower" do
     cwd "#{deploy[:deploy_to]}/current"
     user "root"
-    command "bower install --allow-root"
+    command "bower install --allow-root --config.interactive=false"
     command "gulp default"
   end
 end
