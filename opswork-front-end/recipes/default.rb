c8e328ffa5cb5b7a2d012a513da8a5e0d8791cb1
Johannes Boyne <johannes@boyne.de>
2015-03-03 09:41:16 +0100

    Change order

--- opswork-front-end/recipes/default.rb
+++ opswork-front-end/recipes/default.rb
@@ -1,18 +1,11 @@
 node[:deploy].each do |app, deploy|
-  execute "install deps" do
-    cwd "#{deploy[:deploy_to]}/current"
+  execute "install deps and run setup" do
     user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    command "ln -s /usr/bin/nodejs /usr/bin/node"
     command "npm install -g gulp"
     command "npm install -g bower"
-    command "ln -s /usr/bin/nodejs /usr/bin/node"
-    command "echo \"{'interactive': false}\" > ~/.bowerrc"
-  end
-
-  execute "run gulp and bower" do
-    cwd "#{deploy[:deploy_to]}/current"
-    user "root"
     command "npm install"
-    command "npm i gulp"
     command "bower install --allow-root --config.interactive=false"
     command "gulp default"
   end
