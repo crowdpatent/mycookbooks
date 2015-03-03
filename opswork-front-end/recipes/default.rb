691cf3a32166b2551f9748eeb082758ef1db4c4d
Johannes Boyne <johannes@boyne.de>
2015-03-03 10:15:41 +0100

    Change approach

--- opswork-front-end/recipes/default.rb
+++ opswork-front-end/recipes/default.rb
@@ -1,12 +1,15 @@
 node[:deploy].each do |app, deploy|
-  execute "install deps and run setup" do
+  bash "install deps and run setup" do
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
-    command "ln -s /usr/bin/nodejs /usr/bin/node"
-    command "npm install -g gulp"
-    command "npm install -g bower"
-    command "npm install"
-    command "bower install --allow-root --config.interactive=false"
-    command "gulp default"
+    code <<-EOH
+    ln -s /usr/bin/nodejs /usr/bin/node
+    npm cache clean
+    npm install
+    npm install -g gulp
+    npm install -g bower
+    bower install --allow-root --config.interactive=false
+    gulp default
+    EOH
   end
 end
